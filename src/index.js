15113f25f6d8484b2a4e5ee50c2ff96678ad779c
Francesco Sacchi <depsir@gmail.com>
2019-09-05 22:15:22 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+const App = () => {
+    return <div>hello world</div>
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
+
